b1a814c404871b85265c1bbd620346a1b255dfff
francomacedo96 <francomacedo@outlook.com>
2023-08-16 12:42:30 -0300

    crm

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,8 +3,16 @@ import { Outlet } from "react-router-dom"
 function Layout() {
   return (
     <div className="md:flex md:min-h-screen">
-        <h1 className="text-6xl font-bold">CRM React - react</h1>
-        <Outlet/>
+
+        <div className="md:w-1/4 bg-blue-600 px-5 py-10">
+
+        </div>
+
+        <main className="md:w-3/4 md:h-screen overflow-scroll">
+
+
+        </main>
+
     </div>
   )
 }
