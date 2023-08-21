62c940bb3c976527d77f49512b819bcf0e72011c
francomacedo96 <francomacedo@outlook.com>
2023-08-21 15:53:25 -0300

    nav

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,23 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link, useLocation } from "react-router-dom"
 
 function Layout() {
+
+  const location = useLocation()
+
   return (
     <div className="md:flex md:min-h-screen">
 
-        <aside className="md:w-1/4 bg-blue-600 px-5 py-10">
+        <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+          <h2 className="text-4xl font-black text-center text-white">CRM Cliente</h2>
+          <nav className="mt-10">
+            <Link className={` ${location.pathname === "/" ? "text-blue-300" : " text-white "} text-2xl block mt-2`} to="/">Cliente</Link>
+            <Link className={` ${location.pathname === "/clientes/nuevo" ? "text-blue-300" : " text-white "} text-2xl block mt-2`} to="/clientes/nuevo">Nuevo Cliente</Link>
+          </nav>
 
         </aside>
 
         <main className="md:w-3/4 md:h-screen overflow-scroll">
-
+          <Outlet/>
 
         </main>
 
