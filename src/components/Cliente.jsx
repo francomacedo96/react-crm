c0e17255e3cb25885251d244101b12b7ee7799d7
francomacedo96 <francomacedo@outlook.com>
2023-08-22 10:58:06 -0300

    f

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,25 +1,41 @@
 
 function Cliente({ cliente }) {
-    const {
-        nombre,
-        empresa,
-        email,
-        telefono,
-        id
-    } = cliente
+    const { nombre, empresa, email, telefono, id } = cliente
 
 
     return (
-        <tr>
+        <tr className="border-b">
             <td className="p-6">
-                {nombre}
+                <p className="text-2xl text-gray-800">  {nombre} </p>
+                <p> {empresa} </p>
 
             </td>
             <td className="p-6">
-                {telefono}
+                <p className="text-gray-600">  <span className="text-gray-800 font-bold uppercase"> Email: </span>  {email} </p>
+                <p className="text-gray-600">  <span className="text-gray-800 font-bold uppercase"> Telefono: </span>  {telefono} </p>
+
 
             </td>
+
+            <td className="p-6 flex gap-3">
+                <button
+                    type="button"
+                    className="text-blue-600 hover:text-blue-800 uppercase font-bold text-xs"
+                >
+                    Editar
+                </button>
+                <button
+                    type="button"
+                    className="text-red-600 hover:text-red-800 uppercase font-bold text-xs"
+                >
+                    Eliminar
+                </button>
+            </td>
+
+
         </tr>
-        
+
     )
 }
+
+export default Cliente
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,6 +1,38 @@
+import { useNavigate } from "react-router-dom"
+import Formulario from "../components/Forumulario"
+
 function NuevoCliente() {
+
+  const navigate = useNavigate()
+
   return (
-    <div>NuevoCliente</div>
+
+    <>
+
+      <h1 className="font-black text-4xl text-blue-900">Nuevo clientes</h1>
+      <p className="mt-3">Llena todos los campos para registrar un cliente</p>
+
+      <div className="flex justify-end">
+
+        <button onClick={() => navigate(-1)} className="bg-blue-800 text-white px-3 py-1 font-bold uppercase">
+          Volver
+        </button>
+
+      </div>
+
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-15">
+        <form>
+
+          <Formulario />
+
+          <input type="submit" value="registrar cliente" className="mt-5 w-full bg-blue-800 p-3 text-white text-lg uppercase font-bold" />
+        </form>
+      </div>
+
+    </>
+
+
   )
 }
 
