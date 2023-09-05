4cae80300e6f78c34342094ce8cede4f20659dcb
francomacedo96 <francomacedo@outlook.com>
2023-09-05 15:32:29 -0300

    manejar errores

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,21 @@
+import { useRouteError } from "react-router-dom";
+
+
+export default function ErrorPage( ) {
+
+    const error = useRouteError()
+    console.log(error)
+
+    return (
+
+        <div className="space-y-8">
+            <h1 className="text-center font-extrabold text-6xl mt-20 text-blue-900">CRM - Clientes</h1>
+
+            <h1 className="text-center"> Hay un error </h1>
+
+            <h1 className="text-center"> {error.message} </h1>
+
+        </div>
+
+    )
+}
